bee20d2e3cc84ad69f4b635084e7b70020fe210b
xuhengfeng <564297479@qq.com>
2018-05-02 11:16:49 +0800

    update

--- pages/index/hotHouse.js
+++ pages/index/hotHouse.js
@@ -5,50 +5,55 @@ const app = getApp();
 Page({
   data: {
     houseList: [],
-    page: 1,
+    page: 2,
     currentCity: null,//当前城市
     showload: false,//加载圈
     //热门小区  小区二手房 同小区房源
     contentType: null,
     IPS: [Api.IP_HOTBUILDING, Api.IP_HOTBUILDING, Api.IP_SAMEUSED],
-    num: ''
+    num: '',//切换ip
+    sdid: ''
   },
   onLoad(options) {
+    console.log(options)
     wx.setNavigationBarTitle({title: options.title})
 
     if(options.title == '热门小区'){
       this.setData({
         contentType: 11,
+        sdid: options.id,
         num: 0
       });
     }else if (options.title == '小区二手房') {
       this.setData({ 
         contentType: 22,
+        sdid: options.id,
         num: 1
        });
     }else if(options.title == '同小区房源') {
       this.setData({
         contentType: 33,
+        sdid: options.id,
         num: 2
       });
     }
 
-    //热门小区  小区二手房
+    //热门小区 同小区房源 小区二手房
     wx.getStorage({
       key: 'selectCity',
       success: (res)=> {
         this.setData({currentCity: res.data.value});
-        //同小区房源
-        if (this.data.contentType == 33) {
-          let IP = this.data.IPS[this.data.num] + res.data.value+'/'+options.id
-          let params = {'scity': this.data.currentCity, 'pageNo': 1}
+          let IP = this.data.IPS[this.data.num] + this.data.currentCity + '/'+ this.data.sdid;
+          let params = {'scity': this.data.currentCity, 'pageNo': 1};
           this.getServerData(IP, params);
-        }
       }
     })
   },
   getServerData(IP, params) {
     app.httpRequest(IP, params, (error, data) => {
+      data.data.forEach((item) => {
+        item.houseTag = item.houseTag.split(',');
+      })
       this.setData({houseList: this.data.houseList.concat(data.data)});
     })
   },
@@ -58,6 +63,7 @@ Page({
       'scity': this.data.currentCity,
       'pageNo': page
     }
-    this.getServerData(this.data.IPS[this.data.num], params);
+    let IP = this.data.IPS[this.data.num] + this.data.currentCity + '/'+ this.data.sdid;
+    this.getServerData(IP, params);
   }
 })
