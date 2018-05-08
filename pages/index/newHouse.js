6f971bd844ec97c395a87c3104e7331e5c207ade
xuhengfeng <564297479@qq.com>
2018-05-08 10:59:23 +0800

    add newhouse

--- pages/index/newHouse.js
+++ pages/index/newHouse.js
@@ -4,12 +4,26 @@ Page({
   data: {
     city: [],
     houseList: [],
+    // tabbar  
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    scrollLeft: 0,
   },
   onLoad(options) {
+    wx.getSystemInfo({
+      success: (res)=> {
+          this.setData({
+            winWidth: res.windowWidth,
+            winHieght: res.windowHeight
+          })
+      }
+    })
+    //新房城市信息
     wx.getStorage({
       key: 'currentCity',
       success: (res)=> {
-          //新房城市信息列表
           utils.get(Api.IP_NEWBUILDING + '/' + 'beihai', {
             pageNo: 1
           })
@@ -23,7 +37,34 @@ Page({
     .then((data) => {
       this.setData({ city: data.data });
     })
-   
   },
-
+  //滑动切换tab
+  bindChange(e) {
+      let that = this;
+      that.setData({currentTab: e.detail.current});
+      if(e.detail.current>7){
+        let a = e.detail.current;
+        let query = wx.createSelectorQuery();
+        query.select('.scrollBox').boundingClientRect((res)=>{
+          let b = res.width;
+          console.log(b)
+          that.setData({scrollLeft: (a-6)*75})
+        })
+        query.selectViewport().scrollOffset();
+        query.exec((res)=>{})
+      }else{
+        let a = e.detail.current;
+        this.setData({scrollLeft: 0})
+      }
+  },
+  //点击切换tab
+  swichNav(e) {  
+    var that = this;  
+    console.log(e.target)  
+    if (this.data.currentTab === e.target.dataset.current) {  
+      return false;  
+    } else {  
+      that.setData({currentTab: e.target.dataset.current})  
+    }  
+  },  
 })
