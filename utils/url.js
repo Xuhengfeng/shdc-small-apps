d6ab0ae6dff612bf0ebc9dc34bdf852163db5ad1
xuhengfeng <564297479@qq.com>
2018-05-28 12:24:06 +0800

    update

--- utils/url.js
+++ utils/url.js
@@ -1,6 +1,6 @@
 'use strict';
 const SERVERINDEX = 0;
-const SERVER_IPS = ["http://112.74.181.229:7031/custAppApi"];//api接口
+const SERVER_IPS = ["https://custapis.shyj.cn/custAppApi"];//api接口
 const SERVER_IP = SERVER_IPS[SERVERINDEX];
 
 //首页 接口
