d78fb1d08088555ed094b5ee459ab00293edb2e5
xuhengfeng <564297479@qq.com>
2018-06-07 15:56:53 +0800

    我的收藏

--- pages/mine/collection1.js
+++ pages/mine/collection1.js
@@ -1,63 +1,46 @@
 const Api = require("../../utils/url");
 const utils = require("../../utils/util");
-const filter = require("../../utils/filter.js");
-const sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+const filter = require("../../utils/filter");
 
 Page(filter.loginCheck({
   data: {
-    tabs: ["二手房", "租房", "小区"],
-    activeIndex: 0,
-    sliderOffset: 0,
-    sliderLeft: 0,
+    isOriginHouse: true,//是否原始房源
     houseList: [],
-    hasMore: false,
     showload: false,
-    IPS: [Api.IP_HOUSECOLLECTIONLIST, Api.IP_RENTCOLLECTIONLIST, Api.IP_COLLECTIONLIST],
     num: 0,
     currentCity: null,
     page: 1,
   },
   onLoad() {
-    wx.getSystemInfo({
-      success: (res) => {
-        this.setData({
-          sliderLeft: (res.windowWidth / this.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / this.data.tabs.length * this.data.activeIndex
-        });
-      }
-    });
     utils.storage('selectCity')
     .then((res)=>{
       this.setData({currentCity: res.data.value})
     })
     this.getDataFromServer(0);    
   },
-  tabClick(e) {
-    let num = e.currentTarget.dataset.index;
-    this.getDataFromServer(num);
-    this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id,
-      num: num,
-      page: 1
-    });
-    this.cacheHouseType(this.data.tabs[num]);
-    this.cacheHouseType2(this.data.tabs[num]);
-  },
   getDataFromServer(num) {
-    this.setData({hasMore: true})
     let params = {pageNo: 1,unicode: wx.getStorageSync("userToken")}
-    utils.get(this.data.IPS[num], params)
+    utils.get(Api.IP_HOUSECOLLECTIONLIST, params)
     .then(data => {
       data.data.forEach((item) => {
         item.houseTag = item.houseTag.split(',');
+        this.statusParse(item, item.status);
       })
+      console.log(data.data)
       this.setData({houseList: data.data})
-      this.setData({hasMore: false});
     })
   },
+  //状态检测
+  statusParse(item, num) {
+    console.log(item, num)
+    switch(num){
+      case 0:item.status="正常";break;//正常
+      case 1:item.status="已售";break;//已售
+      case 2:item.status="已失效";break;//已失效
+      case 3:item.status="已停售";break;//已停售
+    }
+  },
   onReachBottom() {
-    this.setData({ hasMore: true })
     let params = {
       scity: this.data.currentCity,
       pageNo: this.data.page++,
@@ -70,15 +53,6 @@ Page(filter.loginCheck({
         item.houseTag = item.houseTag.split(',');
       })
       this.setData({ houseList: this.data.houseList.concat(data.data)})
-      this.setData({ hasMore: false });
     })
-  },
-  //缓存房源类型 可以改变的 
-  cacheHouseType(value) {
-    wx.setStorageSync('houseTypeSelect', value)
-  },
-  //缓存房源类型 不可改变的
-  cacheHouseType2(value) {
-    wx.setStorageSync('onceHouseType', value)
-  },
+  }
 }));
