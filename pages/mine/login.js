9251147bfc1f3630e2ce8c18cdc98ae10ffce095
xuhengfeng <564297479@qq.com>
2018-07-19 13:49:51 +0800

    update

--- pages/mine/login.js
+++ pages/mine/login.js
@@ -171,7 +171,7 @@ Page({
           'Content-Type': 'application/json',
           'scity': this.data.currentCity,
         },
-        methods: "POST",
+        method: "POST",
         success: res=>{
           wx.hideLoading();
           if(res.data.status == 500){
@@ -185,24 +185,7 @@ Page({
                   .then(res=>{})
           }
         }
-
       })
-      // utils.post(Api.IP_GETSMSCODE,params)
-      // .then(data=>{
-      //       console.log(data)
-      //       if(data.data.status == 500){
-      //         let params2 = {
-      //           mobile: mobilePhone,
-      //           sign: md5(key.toUpperCase()),
-      //           operateType: "LOGIN",
-      //           scity: this.data.currentCity
-      //         }
-      //         return utils.post(Api.IP_GETSMSCODE,params2)
-      //       }
-      // })
-      // .then(data2=>{
-      //       console.log(data2)
-      // })
     }
   }
 })
