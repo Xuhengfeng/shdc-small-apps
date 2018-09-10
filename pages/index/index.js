94e503be2a49ab0117f8e6f9224efa007565ff4f
xuhengfeng <564297479@qq.com>
2018-09-10 17:05:32 +0800

    优化代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -343,7 +343,14 @@ Page({
   },
   //页面滚动监听
   onPageScroll(res) {
-    let result = app.oScroll(res);
-    this.setData({scrollNum: result.scrollNum, changeBg: result.changeBg});
+
+    const denominator  = wx.getSystemInfoSync().windowWidth / 375 * 330;
+    let percent = res.scrollTop / denominator;
+    let scrollNum = percent>=1 ? 1 : percent;
+    let changeBg = 'rgba(250,250,250,'+scrollNum+')';
+    if(scrollNum<=0.4){
+      changeBg = "#fff"; 
+    }
+    this.setData({scrollNum: scrollNum, changeBg: changeBg});
   }
 })
