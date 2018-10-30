61e4da6067e20c37d0ba2063cf5ae08041faadb8
xuhengfeng <564297479@qq.com>
2018-10-30 16:00:59 +0800

    update

--- pages/houseDetail/houseDetail2.js
+++ pages/houseDetail/houseDetail2.js
@@ -1,6 +1,7 @@
 const Api = require("../../utils/url");
 const utils = require("../../utils/util");
 import Toast from '../../vant-weapp/dist/toast/toast';
+const app = getApp();
 Page({
   data: {
     scrollNum: 0,
